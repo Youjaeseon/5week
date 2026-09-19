68a81a53103bc07df69bce50b14a5917a2a1b175
Youjaeseon <wotjd0701@naver.com>
2026-09-19 07:12:38 +0000

    04 solve

--- challenges/04_double_free/bug.c
+++ challenges/04_double_free/bug.c
@@ -101,8 +101,7 @@ static void directory_dump(Directory *d) {
 
 static void directory_free(Directory *d) {
     for (int i = 0; i < d->count; i++) {
-        free(d->by_id[i]->name);
-        free(d->by_id[i]);                 
+        free(d->by_id[i]->name);             
     }
     for (int i = 0; i < d->count; i++) {
         free(d->by_name[i]);               
